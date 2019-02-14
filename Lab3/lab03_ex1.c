356a93f9ee6e263992e9f5ca9746931b7ab71ada
pbrodhead <pbrodhead8@gmail.com>
2019-02-14 11:29:21 -0500

    added functionality to program

--- Lab3/lab03_ex1.c
+++ Lab3/lab03_ex1.c
@@ -5,6 +5,8 @@
 int len = 7;
 int nums[7] = {90, 81, 78, 95, 79, 72, 85};
 int r_min;
+int r_max;
+int r_avg;
 
 void * myMin(void * param){
     int i;
@@ -17,10 +19,40 @@ void * myMin(void * param){
     pthread_exit(0);
 }
 
+void * myMax(void * param){
+    r_max = nums[0];
+    for (int i = 0; i < len; i++){
+        if (r_max < nums[i])
+            r_max = nums[i];
+    }
+	
+    pthread_exit(0);
+}
+
+void * myAvg(void * param){
+    r_avg = 0;
+    for (int i = 0; i < len; i++)
+        r_avg += nums[i];
+        
+    r_avg = r_avg / len;
+}
+
 int main(int argc, char** argv){
-    pthread_t tid;
-    pthread_create(&tid, 0, myMin, NULL);
-    pthread_join(tid, NULL);
+    pthread_t tid1;
+    pthread_t tid2;
+    pthread_t tid3;
+    
+    pthread_create(&tid1, 0, myMin, NULL);
+    pthread_create(&tid2, 0, myMax, NULL);
+    pthread_create(&tid3, 0, myAvg, NULL);
+    
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+    
     printf("Min = %d\n", r_min);
+    printf("Max = %d\n", r_max);
+    printf("Avg = %d\n", r_avg);
+    
     return 0;
 }
