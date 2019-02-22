076c6e160807b08f78ade2662cbd435d704a263b
pbrodhead <pbrodhead8@gmail.com>
2019-02-22 18:09:50 -0500

    added file redirecting functionality

--- Project1/main.c
+++ Project1/main.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <wait.h>
 
 #define MAX_LINE 80
 #define MAX_ARGS (MAX_LINE/2 + 1)
@@ -16,11 +20,15 @@ int main(void){
     pid_t pid;
     int background = 0;
     char *hist = "hi";
+    int in, out;
+    int fdin, fdout;
     
     while(should_run){
         printf("osh>");
         fflush(stdout);
-        int i = 0;
+        int numArgs = 0;
+        in = 0;
+        out = 0;
         
         fgets(temp,MAX_LINE,stdin);
         int len = strlen(temp);
@@ -37,13 +45,13 @@ int main(void){
         
         token = strtok(command, s);
         while(token != NULL){
-            args[i] = token;
+            args[numArgs] = token;
             if(!strcmp(token,"&"))
                 background = 1;
             token = strtok(NULL, s);
-            i++;
+            numArgs++;
         }
-        args[i] = NULL;
+        args[numArgs] = NULL;
         if(!strcmp(args[0],"exit")){
             should_run = 0;
             break;
@@ -53,11 +61,41 @@ int main(void){
             chdir(args[1]);
         }
         
+        if(numArgs > 2){
+            if(!strcmp(args[numArgs - 2], ">")){
+                out = 1;
+            }
+            else if(!strcmp(args[numArgs - 2], "<")){
+                in = 1;
+            }
+        }
+        
         hist = malloc(len * sizeof(char));
         memcpy(hist,&temp[0],len);
         
         pid = fork();
         if(pid == 0){
+            if(in){
+                args[numArgs - 2] = NULL;
+                if((fdin = open(args[numArgs - 1], O_RDONLY)) < 0){
+                    printf("Failed to open file\n");
+                    exit(0);
+                }
+                
+                dup2(fdin, 0);
+                close(fdin);
+            }
+            else if(out){
+                args[numArgs - 2] = NULL;
+                if((fdout = creat(args[numArgs - 1], 0644)) < 0){
+                    printf("File error\n");
+                    exit(0);
+                }
+                
+                dup2(fdout, STDOUT_FILENO);
+                close(fdout);
+            }
+            
             execvp(args[0],args);
         }
         else if(pid > 0){
