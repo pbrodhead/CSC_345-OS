ace37078a8e2b8841b4f6f8bf6f4a8468b9861e5
pbrodhead <pbrodhead8@gmail.com>
2019-02-22 17:45:17 -0500

    fixed args, added history and cd

--- Project1/main.c
+++ Project1/main.c
@@ -2,16 +2,20 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 #define MAX_LINE 80
 #define MAX_ARGS (MAX_LINE/2 + 1)
 
 int main(void){
-    char args[MAX_ARGS][MAX_LINE];//command line arguments
+    char *args[MAX_ARGS];//command line arguments
     char temp[MAX_LINE];
     char * token;
     const char s[2] = " ";
     int should_run = 1;   //flag to determine when to exit
+    pid_t pid;
+    int background = 0;
+    char *hist = "hi";
     
     while(should_run){
         printf("osh>");
@@ -19,23 +23,49 @@ int main(void){
         int i = 0;
         
         fgets(temp,MAX_LINE,stdin);
-        token = strtok(temp, s);
+        int len = strlen(temp);
+        
+        char *command = malloc(MAX_ARGS * sizeof(char));
+        
+        if(temp[len - 1] == '\n')
+            temp[len - 1] = '\0'; 
+        
+        if(strcmp(temp,"!!") == 0)
+            command = hist;
+        else
+            command = temp;
+        
+        token = strtok(command, s);
         while(token != NULL){
-            sprintf(args[i],"%s\n",token);
+            args[i] = token;
+            if(!strcmp(token,"&"))
+                background = 1;
             token = strtok(NULL, s);
             i++;
         }
-        
-        //DEBUG
-        for(int j = 0; j < 2; j++){
-            printf("args[%d]: %s",j,args[j]);
-        }
-        printf("%d",strcmp(args[0],"exit"));
-        
-        if(strcmp(args[0],"exit") == 10)
+        args[i] = NULL;
+        if(!strcmp(args[0],"exit")){
             should_run = 0;
-        else
-            printf("That is not a valid command\n");
+            break;
+        }
+        
+        if(!strcmp(args[0],"cd")){
+            chdir(args[1]);
+        }
+        
+        hist = malloc(len * sizeof(char));
+        memcpy(hist,&temp[0],len);
+        
+        pid = fork();
+        if(pid == 0){
+            execvp(args[0],args);
+        }
+        else if(pid > 0){
+            if(!background)
+                wait(NULL);
+        }
+        
+        
     }
     
     return 0;
