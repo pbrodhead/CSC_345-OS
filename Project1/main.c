c89a31d93bb37ace20c63529b0d86e1bafd62043
pbrodhead <pbrodhead8@gmail.com>
2019-02-22 01:14:02 -0500

    implemented arg parsing, ghetto fixed exit command

--- Project1/main.c
+++ Project1/main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_LINE 80
+#define MAX_ARGS (MAX_LINE/2 + 1)
 
 int main(void){
-    char * args[MAX_LINE/2 + 1];//command line arguments
-    int should_run = 1;         //flag to determine when to exit
+    char args[MAX_ARGS][MAX_LINE];//command line arguments
+    char temp[MAX_LINE];
+    char * token;
+    const char s[2] = " ";
+    int should_run = 1;   //flag to determine when to exit
     
     while(should_run){
         printf("osh>");
         fflush(stdout);
+        int i = 0;
         
-        gets(args);
+        fgets(temp,MAX_LINE,stdin);
+        token = strtok(temp, s);
+        while(token != NULL){
+            sprintf(args[i],"%s\n",token);
+            token = strtok(NULL, s);
+            i++;
+        }
         
-        if(strcmp(args,"exit") == 0)
-            break;
+        //DEBUG
+        for(int j = 0; j < 2; j++){
+            printf("args[%d]: %s",j,args[j]);
+        }
+        printf("%d",strcmp(args[0],"exit"));
+        
+        if(strcmp(args[0],"exit") == 10)
+            should_run = 0;
         else
             printf("That is not a valid command\n");
     }
